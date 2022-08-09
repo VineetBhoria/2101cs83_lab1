8fc3b5a23e5a27b2bc72e462ee4aad19476278ef
Vineet Bhoria <BhoriaVineet9@gmail.com>
2022-08-09 22:34:41 +0530

    main function done of calculator

---
+++ calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b,c;
+    printf("WELCOME TO SIMPLE CALCULATOR\n");
+    printf("---------------------------------------------------------------------------------------------------------------------------------\n");
+
+    printf("Enter the first number\n");
+    scanf("%f",&b);
+    printf("Enter the second number\n");
+    scanf("%f",&b);
+
+    printf("Enter + for addition : ");
+    printf("Enter - for subtraction : ");
+    printf("Enter / for division : ");
+    printf("Enter * for multiplicationS : ");
+    
+    switch (a)
+    {
+    case '+':
+        printf("The sum of the two numbers is ");
+        printf(add());
+        break;
+    case '-':
+        printf("The subtration of the two numbers is ");
+        printf(sub());
+        break;
+    case '/':
+        printf("The division of the two numbers is ");
+        printf(div());
+        break;
+    case '*':
+        printf("The multiplication of the two numbers is ");
+        printf(mult());
+        break;
+
+    default:
+        printf("Invalid input");
+        break;
+    }
+    return 0;
+}
